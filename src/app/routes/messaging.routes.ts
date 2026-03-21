8a190e60e138d25450d2f6456dae9477f00cfd86
LaurentMbuyu <laurent.mbuyu@takeda.com>
2026-03-21 22:59:37 +0200

    [New]: Messagerie for pilier

--- src/app/routes/messaging.routes.ts
+++ src/app/routes/messaging.routes.ts
@@ -4,6 +4,6 @@ export const messagingRoutes: Routes = [
   {
     path: 'messagerie',
     loadComponent: () => import('../pages/messagerie/messagerie.component').then((m) => m.MessagerieComponent),
-    data: { roles: ['ADMIN', 'RECEPTION', 'SERVICE_INTERNE', 'CHEF_SG', 'ASSISTANT_CHEF'], preload: true }
+    data: { roles: ['ADMIN', 'RECEPTION', 'SERVICE_INTERNE', 'CHEF_SG', 'ASSISTANT_CHEF', 'PILIER'], preload: true }
   }
 ];
